d1b8f8ec176c2c4194f91b251a1002a12f5dfc47
hito <26297679+hitowaft@users.noreply.github.com>
2019-06-04 20:40:49 +0900

    fix bug, add parentheses to sys.exit()

--- delete_negative_tweet.py
+++ delete_negative_tweet.py
@@ -79,7 +79,7 @@ tw_id_and_text_df = tweet_df.reindex(columns=["timestamp", "tweet_id", "text"])
 print("消したい単語を入力してください。複数の単語を入力するにはスペースで区切る（cを入力でキャンセル）")
 input_words = input().split()
 if input_words == "c" or "C":
-    sys.exit
+    sys.exit()
 else:
     pass
 
